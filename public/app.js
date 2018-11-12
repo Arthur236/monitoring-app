f985c6cf24e7267a40293aebafb03cadee075267
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2018-11-12 13:09:53 +0300

    Allow a user to create a check

--- public/app.js
+++ public/app.js
@@ -148,18 +148,35 @@ app.bindForms = function() {
 
         for(let i = 0; i < elements.length; i++) {
           if(elements[i].type !== 'submit') {
-            const valueOfElement = elements[i].type === 'checkbox' ? elements[i].checked : elements[i].value;
-            if(elements[i].name === '_method') {
+            // Determine class of element and set value accordingly
+            const classOfElement = typeof(elements[i].classList.value) === 'string' && elements[i].classList.value.length > 0 ? elements[i].classList.value : '';
+            const valueOfElement = elements[i].type === 'checkbox' && classOfElement.indexOf('multiselect') === -1 ? elements[i].checked : classOfElement.indexOf('intval') == -1 ? elements[i].value : parseInt(elements[i].value);
+            const elementIsChecked = elements[i].checked;
+            // Override the method of the form if the input's name is _method
+            let nameOfElement = elements[i].name;
+            if(nameOfElement === '_method') {
               method = valueOfElement;
             } else {
-              payload[elements[i].name] = valueOfElement;
-            }
+              // Create an payload field named "method" if the elements name is actually httpmethod
+              if(nameOfElement === 'httpmethod') {
+                nameOfElement = 'method';
+              }
+              // If the element has the class "multiselect" add its value(s) as array elements
+              if(classOfElement.indexOf('multiselect') > -1) {
+                if(elementIsChecked) {
+                  payload[nameOfElement] = typeof(payload[nameOfElement]) === 'object' && payload[nameOfElement] instanceof Array ? payload[nameOfElement] : [];
+                  payload[nameOfElement].push(valueOfElement);
+                }
+              } else {
+                payload[nameOfElement] = valueOfElement;
+              }
 
+            }
           }
         }
 
         // If the method is DELETE, the payload should be a queryStringObject instead
-        const queryStringObject = method == 'DELETE' ? payload : {};
+        const queryStringObject = method === 'DELETE' ? payload : {};
 
         // Call the API
         app.client.request(undefined, path, method, queryStringObject, payload, function(statusCode, responsePayload) {
@@ -236,6 +253,11 @@ app.formResponseProcessor = function(formId, requestPayload, responsePayload) {
   if(formId === 'accountEdit3') {
     app.logUserOut(false);
     window.location = '/account/deleted';
+  }
+
+  // If the user just created a new check successfully, redirect back to the dashboard
+  if(formId === 'checksCreate') {
+    window.location = '/checks/all';
   }
 
 };
